6695cd7f902686f60d5e8df5876214daba2c5a2f
Isaias Julio <isaiasjulio@isaiass-air-2.myfiosgateway.com>
2024-01-01 03:38:09 -0500

    GpComplete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react" 
+import ButtonUsage from "./components/button"
+import './App.css'
+import 'typeface-roboto';
+import MyCheckbox from './components/checkedbox'
+
+
+export default function App() {
+ 
+
+  return (
+    <>
+      <div>
+        
+        <ButtonUsage/>
+        <MyCheckbox/>
+        
+      </div>
+   
+    </>
+  )
+}
+
